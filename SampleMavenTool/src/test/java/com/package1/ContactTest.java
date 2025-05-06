0f23ef37c220f045099121ed319b7931b280569d
sajarikharvatkar8@gmail.com <sajarikharvatkar8@gmail.com>
2025-05-06 11:33:21 +0530

    sample maven project is added

---
+++ SampleMavenTool/src/test/java/com/package1/ContactTest.java
@@ -0,0 +1,13 @@
+package com.package1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+public void m1()
+{
+		String browser=System.getProperty("Browser");
+		System.out.println(browser);
+	System.out.println("create contact");
+	}
+}
---
+++ SampleMavenTool/src/test/java/com/package2/OrgTest.java
@@ -0,0 +1,12 @@
+package com.package2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	@Test
+	public void m2()
+	{
+		System.out.println("create org");
+	}
+
+}
